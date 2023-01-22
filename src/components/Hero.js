f0999583750afb15542b8b4b4d07b39d2430acba
Josh Hashimoto <hashimotojosh@gmail.com>
2023-01-21 16:31:13 -0800

    hero section (filler text)

---
+++ src/components/Hero.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div className="my-10 bg-black py-5 px-5">
+      <div className="flex flex-col text-white">
+        <h1 className="text-4xl text-green-700 font-semibold">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </h1>
+        <p className="mt-4 text-2xl">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import Hero from "../components/Hero.js";
 function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1 className="text-4xl">JourneyShare</h1>
+      <Hero />
       <Link to="/events">
         <h1>Events</h1>
       </Link>
