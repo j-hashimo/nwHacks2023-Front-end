61a4f7da4b758f747516102f19dd2b944886b59d
Josh Hashimoto <hashimotojosh@gmail.com>
2023-01-22 09:29:02 -0800

    fixed logout button and signup links

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,13 +1,15 @@
-import React, { useState, useEffect } from 'react'
-import "./LoginForm.css"
+import React, { useState, useEffect } from "react";
+import "./LoginForm.css";
 import { useDispatch, useSelector } from "react-redux";
 import { login, logout } from "../redux/useSlice";
-
+import { Link } from "react-router-dom";
 function LoginForm() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const { error, isLoading, user, isAuthenticated } = useSelector((state) => state.auth);
+  const { error, isLoading, user, isAuthenticated } = useSelector(
+    (state) => state.auth
+  );
 
   const dispatch = useDispatch();
 
@@ -21,36 +23,51 @@ function LoginForm() {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    dispatch(login({email, password}))
-
-  }
+    dispatch(login({ email, password }));
+  };
 
   const checkFun = () => {
-    console.log(user, isAuthenticated)
-  }
+    console.log(user, isAuthenticated);
+  };
 
   const logoutFun = () => {
-    dispatch(logout())
-  }
-
+    dispatch(logout());
+  };
 
   return (
     <>
-      <button onClick={checkFun}>Click</button>
-      <button onClick={logoutFun}>Logout</button>
-      <form onSubmit={handleSubmit} className='LoginForm'>
-        <h1 className='LoginFormPrompt'> Sign in </h1>
-        <h2 className='LoginFormText'> Email </h2>
-        <input type="text" className='UserEmail' value={email} onChange={(e) => setEmail(e.target.value)} />
-        <h2 className='LoginFormText'> Password </h2>
-        <input type="text" className='UserPassword'
-          value={password} onChange={(e) => setPassword(e.target.value)}
+      <Link to="/">
+        <div>
+          <button onClick={checkFun}>Click</button>
+          <button onClick={logoutFun}>Logout</button>
+        </div>
+      </Link>
+      <form onSubmit={handleSubmit} className="LoginForm">
+        <h1 className="LoginFormPrompt"> Sign in </h1>
+        <h2 className="LoginFormText"> Email </h2>
+        <input
+          type="text"
+          className="UserEmail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
-        <label htmlFor='LoginButton' className='LoginFormText'> Press the button below to log in </label>
-        <button type='submit' className='LoginButton' >Login</button>
+        <h2 className="LoginFormText"> Password </h2>
+        <input
+          type="text"
+          className="UserPassword"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <label htmlFor="LoginButton" className="LoginFormText">
+          {" "}
+          Press the button below to log in{" "}
+        </label>
+        <button type="submit" className="LoginButton">
+          Login
+        </button>
       </form>
     </>
-  )
+  );
 }
 
-export default LoginForm
+export default LoginForm;
